aa8fd6c60be5562ab708b87bbcf39e1eb721e036
Sarah Hudspeth <shhudspeth@gmail.com>
2020-07-08 16:14:45 -0700

    completed all but divide

--- calculator.py
+++ calculator.py
@@ -9,31 +9,32 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 play = True
 while play == True:
     express = input("Please enter your math expression: ")
-    num2 = express.split(" ")
-    if len(num2) == 1:
-        if num2[0] == "q":
+    input_token = express.split(" ")
+    if len(input_token) == 1:
+        if input_token[0] == "q":
             play = False
-            print("Thanks for using this program")
         else:
             Print("invalid expression")
-    elif len(num2) == 2:
-        if num2[0].lower() == "square":
+
+    elif len(input_token) == 2:
+        num1 = float(input_token[1])
+        if input_token[0].lower() == "square":
             print(square(num1))
-        elif num2[0].lower() == "cube":
+        elif input_token[0].lower() == "cube":
             print(cube(num1))
         else:
             print("invalid expression. try again please!")
 
-    elif len(num2) == 3:
-        num1 = float(num2[1])
-        num2 = float(num2[2])
+    elif len(input_token) == 3:
+        num1 = float(input_token[1])
+        num2 = float(input_token[2])
 
-        if num2[0].startswith('+'):
+        if input_token[0].startswith('+'):
             print(add(num1, num2))
-        elif num2[0].startswith('-'):
+        elif input_token[0].startswith('-'):
             print(subtract(num1, num2))
         elif input_token[0].startswith('*'):
-            print(multipy(num1, num2))
+            print(multiply(num1, num2))
         elif input_token[0].startswith('pow'):
             print(pow(num1, num2))
         elif input_token[0].startswith('mod'):
